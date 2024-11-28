653ad0ed5635fff940825e5dfdfc255c401e22ed
vanessa.machordom <vanessa.machordom@f1-connecting.com>
2024-11-28 15:16:39 +0100

    modelos

---
+++ Models/Localidad.cs
@@ -0,0 +1,9 @@
+namespace MyProject.Models
+{
+    public class Localidad
+    {
+        public int IdLocalidad { get; set; }
+        public string Nombre { get; set; }
+        public int IdProvincia { get; set; }
+    }
+}
---
+++ Models/Monumento.cs
@@ -0,0 +1,15 @@
+namespace MyProject.Models
+{
+    public class Monumento
+    {
+        public int IdMonumento { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public int CodigoPostal { get; set; }
+        public double Longitud { get; set; }
+        public double Latitud { get; set; }
+        public string Descripcion { get; set; }
+        public string Tipo { get; set; }
+        public int IdLocalidad { get; set; }
+    }
+}
