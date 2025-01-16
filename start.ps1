d226c23f74d5319dc54093cb5b2b4a112d01c67e
Aarón Ortiz García <AORTGAR1@upv.edu.es>
2025-01-16 19:12:39 +0100

    Añadido al start Start-Process "dotnet" "run --project BusquedaAPI/BusquedaAPI.csproj"

--- start.ps1
+++ start.ps1
@@ -1,5 +1,6 @@
 # Iniciar los proyectos
 Start-Process "dotnet" "run --project CargaAPI/CargaAPI.csproj"
+Start-Process "dotnet" "run --project BusquedaAPI/BusquedaAPI.csproj"
 Start-Process "dotnet" "run --project WrapperXML/WrapperXML.csproj"
 Start-Process "dotnet" "run --project WrapperJSON/WrapperJSON.csproj"
 Start-Process "dotnet" "run --project WrapperCSV/WrapperCSV.csproj"
